939317f681fd428de7506b2659dcd4f86a721cb6
Thomalet <thomas.malet@club-internet.fr>
2018-10-15 11:35:27 +0200

    Mise à Jour de "Game"

--- src/goosegame/Game.java
+++ src/goosegame/Game.java
@@ -1,20 +1,28 @@
 package goosegame;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-	protected List thePlayers;
+	protected List<Player> thePlayers;
 	protected Board board;
 	
 	public Game(Board board) {
-		
+		this.board = board;
+		this.thePlayers = new ArrayList();
 	}
 	
 	public void addPlayer(Player p) {
-		
+		this.thePlayers.add(p);
 	}
 	
 	public void play() {
-		
+		Player player = this.thePlayers.get(0);
+		Boolean continueGame = true;
+		while (continueGame == true) {
+			if (player.getClass().canBeLeft()==true) {
+				
+			}
+		}
 	}
 }
