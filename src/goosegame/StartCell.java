99f171f8c71608b3e094259952cefad539b1948c
Thomalet <thomas.malet@club-internet.fr>
2018-10-15 10:48:05 +0200

    Mise à jour de "StartCell"

--- src/goosegame/StartCell.java
+++ src/goosegame/StartCell.java
@@ -4,37 +4,30 @@ public class StartCell implements Cell {
 
 	@Override
 	public boolean canBeLeft() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public int getIndex() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int handleMove(int diceThrow) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public boolean isBusy() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void welcomePlayer(Player player) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public Player getPlayer() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
